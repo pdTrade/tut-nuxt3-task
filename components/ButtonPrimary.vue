f32c57bc9c1c022de632c74ab56374992e0a115a
pdTrade <pd.pub.dev@gmail.com>
2024-01-26 14:14:19 +0900

    ボタンコンポーネント

---
+++ components/ButtonPrimary.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  onClick: Function;
+}>();
+</script>
+
+<template>
+  <button class="
+    bg-blue-200
+    rounded-md 
+    p-1 
+    text-md
+    shadow-md 
+    hover:opacity-80"
+    @click="onClick()"
+  >
+    <slot />
+  </button>
+</template>
