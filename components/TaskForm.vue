2f77e4d8d687e8a70fce7e6474bc6027f80d67db
pdTrade <pd.pub.dev@gmail.com>
2024-01-27 17:17:44 +0900

    タスク登録フォームの作成

--- components/TaskForm.vue
+++ components/TaskForm.vue
@@ -29,36 +29,51 @@ const addItem = () => {
   });
 };
 
+const removeItem = (index: number) => {
+  formData.value.items.splice(index, 1);
+};
 
-const submit = () => { };
+
+const submit = () => { 
+  navigateTo('/');
+};
 
 </script>
 
 <template>
-  <div class="">
+  <div class="grid gap-4">
     <label>
       タスク名
       <InputText v-model="formData.name"></InputText>
     </label>
     <div
       v-for="(item, index) in formData.items"
+      class="bg-cyan-100 rounded-md p-4 shadow-md"
     >
-      <label>
-        手順{{ index + 1 }}
-        <InputText v-model="item.name"/>
-      </label>
-      <label>
-        想定時間{{ index + 1 }}
-        <InputNum v-model="item.amount"/>
-      </label>
-      <label>
-        時間単位{{ index + 1 }}
-        <InputText v-model="item.unit"/>
-      </label>
+      <div class="text-right">
+        <ButtonDanger :on-click="() => removeItem(index)">手順{{ index + 1 }}を削除する</ButtonDanger>
+      </div>
+      <div class="grid gap-2 sm:grid-cols-3">
+        <label>
+          手順{{ index + 1 }}
+          <InputText v-model="item.name"/>
+        </label>
+        <label>
+          想定時間{{ index + 1 }}
+          <InputNum v-model="item.amount"/>
+        </label>
+        <label>
+          時間単位{{ index + 1 }}
+          <InputText v-model="item.unit"/>
+        </label>
+      </div>
     </div>
     <div>
       <ButtonPrimary :onClick="addItem">追加</ButtonPrimary>
     </div>
+    <label>
+      内容
+    </label>
     <div class="text-right">
       <ButtonPrimary :onClick="submit">保存</ButtonPrimary>
     </div>
